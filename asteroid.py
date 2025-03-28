b42d754b47329c866487980f057ff0ce63d2c023
kimvwijnen <kimvwijnen@gmail.com>
2025-03-28 15:35:14 +0100

    Bigger asteroids split into 2 smaller asteroids when shot, small asteroids disappear

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 import pygame
 from circleshape import CircleShape
+from constants import *
+import random
 
 
 class Asteroid(CircleShape):
@@ -13,3 +15,22 @@ class Asteroid(CircleShape):
         
     def update(self, dt):
         self.position += (self.velocity * dt)
+
+    def split(self):
+        self.kill()
+
+        # if small asteroid, it disappears 
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        # if not, 2 smaller asteroids appear
+
+        # randomize the angle for the 2 new asteroids
+        new_angle = random.uniform(20, 50)        
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        # initialize the 2 new asteroids
+        asteroid_a = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_a.velocity = self.velocity.rotate(-new_angle) * 1.2
+        asteroid_b = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_b.velocity = self.velocity.rotate(new_angle) * 1.2
--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def main():
 
             for shot in shots:
                 if asteroid.check_collision(shot):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
 
         screen.fill("black")
